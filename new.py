58904dd00e1fea027eb1a5bc638ef5bbfed3e446
Shradha <shradharanikadam26@gmail.com>
2019-11-16 11:58:51 +0530

    new

---
+++ new.py
@@ -0,0 +1,28 @@
+# Str
+
+Str1 = "Codekul"
+Str2 = 'Codekul'
+
+# 'Codekul'- "The Gurukul for coders"!
+Str3 = """'Codekul' - "The Gurukul for coders"!"""
+Str4 = """ Codekul 
+    The
+        Gurukul
+            for
+                coders!"""
+Str5 = "Best training institude: " + Str1       
+
+print(Str1)
+print(Str2)
+print(Str3)
+print(Str4)
+print(Str5)
+
+# list
+
+list1 = [1,2,3,4,5,6,7,8,9]
+list1.remove(5)
+list1.reverse()
+
+
+print(list1)
